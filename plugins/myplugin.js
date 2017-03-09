eaa4ad0e4fedc7b9143292f324d269dea93d1097
Eugene <ebaskiv@gmail.com>
2017-03-09 16:06:54 +0200

    add tamplate code js

--- plugins/myplugin.js
+++ plugins/myplugin.js
@@ -1,20 +1,22 @@
 	
-function tubsInitialization(wrapId) {
-	$('#' + wrapId + ">div>div:not(:first)").hide();
+function tubsInitialization(tabsWrapId) {
+	$('#' + tabsWrapId + " .tabs-content>div:not(:first)").hide();
 	
-	$('#' + wrapId +" .tabs li").each(function(i) {$(this).attr('data-tab', 'tab_' + i)});
-	$('#' + wrapId +" .tabs-content>div").each(function(i) {$(this).attr('data-tab', 'tab_' + i)});
+	$('#' + tabsWrapId +" .tabs li").each(function(i) {$(this).attr('data-tab', 'tab_' + i)});
+	$('#' + tabsWrapId +" .tabs-content>div").each(function(i) {$(this).attr('data-tab', 'tab_' + i)});
 
-	$('#' + wrapId + ' .tabs li').click(function(e){
+	$('#' + tabsWrapId + ' .tabs li').click(function(e){
 		if ($(this).hasClass('active')) return;
 
-		$('#' + wrapId +' .tabs li').removeClass('active');
+		$('#' + tabsWrapId +' .tabs li').removeClass('active');
 		$(this).addClass('active');
 
-		$('#' + wrapId +' .tabs-content>div').hide();
-		$('#' + wrapId +' .tabs-content>div[data-tab=' + $(this).data('tab') + ']').show();
+		$('#' + tabsWrapId +' .tabs-content>div').hide();
+		$('#' + tabsWrapId +' .tabs-content>div[data-tab=' + $(this).data('tab') + ']').show();
 	});
-	/***tabs structure***/ 
+	/**
+	**tabs structure**
+	**/ 
 	// 	<div id="tabs-wrap">
 	// 	  <ul class="tabs">
 	// 		<li class='active'>tab1</li>
@@ -26,6 +28,10 @@ function tubsInitialization(wrapId) {
 	//    </div>
 	//  </div> 
 };
+
+
+
 $(function(e) {
 	tubsInitialization("tabs-wrap");
+	tubsInitialization("tabs-wrap1");
 })
